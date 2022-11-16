f5b861e0bdce48e7c2d9454e1ec9f0f5f5b56796
Guilherme Vallejo <117021313+VallejoGuilherme@users.noreply.github.com>
2022-11-16 11:28:35 +0000

    Add files via upload

---
+++ tipCalculator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Tip Calculator/n")
+bill = input("What was the total bill? ")
+tip = input("What percentage tipo would you like to give? 10, 12 or 15? ")
+people = input("How many people do you want to split? ")
+
+percentage = (float(bill) * float(tip)) / 100
+pay_per_person = float(bill) / float(people)
+all_bill_whith_tips = pay_per_person + percentage / float(people)
+round_number = round(all_bill_whith_tips)
+print(f"Each person should pay {round_number:.2f}")
